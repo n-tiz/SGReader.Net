27a6afbf003eb77e2dee7d4a9dd1f6b0b719143b
salido-m <maxime.salido@gmail.com>
2021-10-22 15:07:59 +0200

    Adding CLI project

---
+++ src/SGReader.CLI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SGReader.Core;
+
+namespace SGReader.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                string fileName = args.SingleOrDefault();
+                var loader = new SGLoader();
+                loader.Load(fileName);
+            }
+        }
+    }
+}
